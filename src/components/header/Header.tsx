b0587a60171382e1c6c40be677da3fa65f2fab64
VardgeM <v.mirzakhanyann@gmail.com>
2025-01-07 02:46:31 +0400

    temporarily hide any header tabs

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,27 @@
-import {AppBar, Box, Toolbar} from '@mui/material'
+import {AppBar, Box, IconButton, Toolbar} from '@mui/material'
 import {Icon} from '@iconify/react';
-import MobileAppBar from '../appBars/mobileAppBar';
-import MainAppBar from '../appBars/appBar';
+// import MobileAppBar from '../appBars/mobileAppBar';
+// import MainAppBar from '../appBars/appBar';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+    const navigate = useNavigate();
 
     return (
         <AppBar sx={{backgroundColor: 'rgba(19,19,19,0.9)', width: '100%'}} position='fixed'>
             <Toolbar>
-                <MobileAppBar/>
-                <MainAppBar/>
+                {/* <MobileAppBar/>
+                <MainAppBar/> */}
+                <Box flexGrow={1} >
+                <IconButton onClick={() => { navigate('/') }} color='inherit' aria-label='logo' sx={{ display: 'flex', flexDirection: 'row', gap: '10px', }}>
+                     <img
+                        src="/yvn-logo.svg"
+                        alt=""
+                        width='160'
+                        height='60'
+                    />
+                </IconButton>
+            </Box>
                 <Box sx={{color: '#1970C7', border: 1, p: 0.5, height: 40, borderRadius: '10px'}}
                      onClick={() => window.location.href = `tel:+37499115211`}
                 >
